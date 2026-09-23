62bda4004691dd4cd35cc62bb65b3bf333a3c766
slim <slimslim80@mail.ru>
2026-09-23 12:28:49 +0300

    develop проверил clang

--- tests/test_args.c
+++ tests/test_args.c
@@ -1,5 +1,5 @@
-#include "minunit.h"
 #include "../src/args.h"
+#include "minunit.h"
 
 static int test_null_options(void);
 static int test_dash_options(void);
@@ -35,13 +35,14 @@ static int test_file_names_dushes_options(void) {
     char *argv[] = {"file_name", "filename1", "-", "filename2", NULL};
     int argc = sizeof(argv) / sizeof(argv[0]) - 1;  // NULL в argc не входит
 
-    char *expected_argv[] = {"filename1", "-", "filename2",};
+    char *expected_argv[] = {"filename1", "-", "filename2"};
     int expected_file_count = sizeof(expected_argv) / sizeof(expected_argv[0]);
-    
+
     ParseArgs(argc, argv, &opts);
     mu_assert_inteq(expected_file_count, opts.file_count);
-    for (int i = 0; i < expected_file_count; i++)
+    for (int i = 0; i < expected_file_count; i++) {
         mu_assert_streq(expected_argv[i], opts.files[i]);
+    }
 
     OptionsFree(&opts);
 
@@ -56,11 +57,12 @@ static int test_dushes_file_names_options(void) {
 
     char *expected_argv[] = {"-", "filename1", "-", "filename2", "-"};
     int expected_file_count = sizeof(expected_argv) / sizeof(expected_argv[0]);
-    
+
     ParseArgs(argc, argv, &opts);
     mu_assert_inteq(expected_file_count, opts.file_count);
-    for (int i = 0; i < expected_file_count; i++)
+    for (int i = 0; i < expected_file_count; i++) {
         mu_assert_streq(expected_argv[i], opts.files[i]);
+    }
 
     OptionsFree(&opts);
 
@@ -75,11 +77,12 @@ static int test_two_file_names_options(void) {
 
     char *expected_argv[] = {"filename1", "filename2"};
     int expected_file_count = sizeof(expected_argv) / sizeof(expected_argv[0]);
-    
+
     ParseArgs(argc, argv, &opts);
     mu_assert_inteq(expected_file_count, opts.file_count);
-    for (int i = 0; i < expected_file_count; i++)
+    for (int i = 0; i < expected_file_count; i++) {
         mu_assert_streq(expected_argv[i], opts.files[i]);
+    }
 
     OptionsFree(&opts);
 
@@ -94,11 +97,12 @@ static int test_one_file_name_options(void) {
 
     char *expected_argv[] = {"filename1"};
     int expected_file_count = sizeof(expected_argv) / sizeof(expected_argv[0]);
-    
+
     ParseArgs(argc, argv, &opts);
     mu_assert_inteq(expected_file_count, opts.file_count);
-    for (int i = 0; i < expected_file_count; i++)
+    for (int i = 0; i < expected_file_count; i++) {
         mu_assert_streq(expected_argv[i], opts.files[i]);
+    }
 
     OptionsFree(&opts);
 
@@ -113,11 +117,12 @@ static int test_two_dash_options(void) {
 
     char *expected_argv[] = {"-", "-"};
     int expected_file_count = sizeof(expected_argv) / sizeof(expected_argv[0]);
-    
+
     ParseArgs(argc, argv, &opts);
     mu_assert_inteq(expected_file_count, opts.file_count);
-    for (int i = 0; i < expected_file_count; i++)
+    for (int i = 0; i < expected_file_count; i++) {
         mu_assert_streq(expected_argv[i], opts.files[i]);
+    }
 
     OptionsFree(&opts);
 
@@ -132,11 +137,12 @@ static int test_dash_options(void) {
 
     char *expected_argv[] = {"-"};
     int expected_file_count = sizeof(expected_argv) / sizeof(expected_argv[0]);
-    
+
     ParseArgs(argc, argv, &opts);
     mu_assert_inteq(expected_file_count, opts.file_count);
-    for (int i = 0; i < expected_file_count; i++)
+    for (int i = 0; i < expected_file_count; i++) {
         mu_assert_streq(expected_argv[i], opts.files[i]);
+    }
 
     OptionsFree(&opts);
 
@@ -151,11 +157,12 @@ static int test_null_options(void) {
 
     char *expected_argv[] = {"-"};
     int expected_file_count = sizeof(expected_argv) / sizeof(expected_argv[0]);
-    
+
     ParseArgs(argc, argv, &opts);
     mu_assert_inteq(expected_file_count, opts.file_count);
-    for (int i = 0; i < expected_file_count; i++)
+    for (int i = 0; i < expected_file_count; i++) {
         mu_assert_streq(expected_argv[i], opts.files[i]);
+    }
 
     OptionsFree(&opts);
 
